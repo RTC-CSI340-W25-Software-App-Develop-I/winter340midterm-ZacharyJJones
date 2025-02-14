25d043cbc14c7fe36bd105d9f48693394f4c6e4f
Zack Jones <81715488+ZacharyJJones@users.noreply.github.com>
2025-02-13 22:16:02 -0800

    added mocha and finished midterm

--- src/index.js
+++ src/index.js
@@ -2,28 +2,88 @@
 import { calculateStarAverage } from "../src/logic.js";
 
 const reviews = [
-  {
-    username: "Rose",
-    image: "./images/rose.png",
-    star: 5,
-    review: "Great coffee and ambiance",
-  },
-  {
-    username: "butterfly2222",
-    image: "./images/avatar2.png",
-    star: 3,
-    review: "Coffee was okay, took way to long to make.",
-  },
-  {
-    username: "Sandy Tuna",
-    image: "./images/avatar1.png",
-    star: 1,
-    review:
-      "The coffee was great but the employees didn't let me stay past closing! ): Worst experience ever.",
-  },
+	{
+		username: "Rose",
+		image: "./images/rose.png",
+		star: 5,
+		review: "Great coffee and ambiance",
+	},
+	{
+		username: "butterfly2222",
+		image: "./images/avatar2.png",
+		star: 3,
+		review: "Coffee was okay, took way to long to make.",
+	},
+	{
+		username: "Sandy Tuna",
+		image: "./images/avatar1.png",
+		star: 1,
+		review:
+			"The coffee was great but the employees didn't let me stay past closing! ): Worst experience ever.",
+	},
 ];
 /////////////////////////////////////////////////////////////////////
 
+// Midterm Deliverable #1: Create a function that will render a review
+function renderReview(review) {
+	/* Output template
+  <div class="review_container">
+    <img src="img url here"/>
+    <div>
+      <p> username here </p>
+      <p> star rating here </p>
+      <p> review here </p>
+    <div>
+  </div>
+  */
+	const innerDiv = document.createElement("div");
+
+	const userName = document.createElement("p");
+	userName.textContent = review.username;
+
+	const starRating = document.createElement("p");
+	starRating.textContent =
+		"".padStart(review.star, "★") + "".padStart(5 - review.star, "☆");
+
+	const reviewText = document.createElement("p");
+	reviewText.textContent = review.review;
+
+	innerDiv.append(userName, starRating, reviewText);
+
+	const reviewContainer = document.createElement("div");
+	reviewContainer.className = "review_container";
+
+	const img = document.createElement("img");
+	img.src = review.image;
+
+	reviewContainer.append(img, innerDiv);
+
+	// place reviews in <section class="reviews"> tag
+	const reviewListArea = document.querySelector(".reviews");
+	reviewListArea.append(reviewContainer);
+
+	// and the rating field
+	const locationStarRating = document.querySelector(".starRating");
+	locationStarRating.textContent =
+		"Star Rating: " + calculateStarAverage(reviews);
+}
+
 //1. Append the reviews to the DOM
+reviews.forEach((x) => renderReview(x));
 
 //2. Append new reviews to the DOM from the form
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const newReview = {
+		username: e.target.username.value,
+		image: e.target.image.value,
+		star: e.target.star.value,
+		review: e.target.review.value,
+	};
+
+	reviews.push(newReview);
+	renderReview(newReview);
+
+	// would be nice to clear the input fields afterwards
+});
--- test/test.js
+++ test/test.js
@@ -3,23 +3,58 @@ import { assert } from "chai";
 import { calculateStarAverage } from "../src/logic.js";
 
 const mockReviews = [
-  {
-    username: "Rose",
-    image: "./images/rose.png",
-    star: 5,
-    review: "Great coffee and ambiance",
-  },
-  {
-    username: "butterfly2222",
-    image: "./images/avatar2.png",
-    star: 3,
-    review: "Coffee was okay, took way to long to make.",
-  },
-  {
-    username: "Sandy Tuna",
-    image: "./images/avitar1.png",
-    star: 1,
-    review:
-      "The coffee was great but the employees didn't let me stay past closing! ): Worst experience ever.",
-  },
+	{
+		username: "Rose",
+		image: "./images/rose.png",
+		star: 5,
+		review: "Great coffee and ambiance",
+	},
+	{
+		username: "butterfly2222",
+		image: "./images/avatar2.png",
+		star: 3,
+		review: "Coffee was okay, took way to long to make.",
+	},
+	{
+		username: "Sandy Tuna",
+		image: "./images/avitar1.png",
+		star: 1,
+		review:
+			"The coffee was great but the employees didn't let me stay past closing! ): Worst experience ever.",
+	},
 ];
+
+describe("Mock Review Test", () => {
+	it("Average is correctly calculated with mock review set", () => {
+		assert.equal(3, calculateStarAverage(mockReviews));
+	});
+});
+
+describe("Average of same number", () => {
+	const testValue = 4;
+	const testArrayLength = 260;
+
+	it("Many of the same number averaged should be that number", () => {
+		const testArray = [];
+		for (let i = 0; i < testArrayLength; i++) {
+			testArray.push({ star: testValue });
+		}
+		assert.equal(testValue, calculateStarAverage(testArray));
+	});
+});
+
+// Made this test to check behavior between webpage and calculateStarAverage. Not sure where the problem is.
+describe("Regression: Webpage displays large number", () => {
+	const testArray = [
+		{ star: 5 },
+		{ star: 3 },
+		{ star: 1 },
+		{ star: 2 },
+		{ star: 2 },
+		{ star: 2 },
+	];
+
+	it("Non-averaged number seen on webpage with this input", () => {
+		assert.equal(1537, calculateStarAverage(testArray));
+	});
+});
